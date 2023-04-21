3e398a99538ebe4ffb8dec8e237e85e5b6a66f6c
Hariram Sankaran <56744845+ramramhariram@users.noreply.github.com>
2023-04-20 17:25:16 -0700

    adding additional intentions

--- modules/hcp-ecs-client/intentions.tf
+++ modules/hcp-ecs-client/intentions.tf
@@ -84,3 +84,13 @@ resource "consul_intention" "all2" {
   destination_namespace = "az2"
   action           = "allow"
 }
+
+#adding second intention for AZ2/namespace
+resource "consul_intention" "all2" {
+  source_name      = "*"
+  source_namespace = "az2"
+  destination_name = "*"
+  destination_namespace = "az2"
+  action           = "allow"
+}
+
