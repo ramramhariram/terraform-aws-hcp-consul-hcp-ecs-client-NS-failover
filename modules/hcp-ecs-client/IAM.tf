9d5dd0e81a891e252631e075478979f1a96082ce
Hariram Sankaran <56744845+ramramhariram@users.noreply.github.com>
2023-04-25 14:20:25 -0700

    adding missing variables

--- modules/hcp-ecs-client/IAM.tf
+++ modules/hcp-ecs-client/IAM.tf
@@ -77,3 +77,56 @@ resource "aws_iam_role_policy_attachment" "hashicups" {
 data "aws_caller_identity" "current" {}
 
 
+
+  variable "iam_service_principals" {
+  type        = map(string)
+  description = "Names of the Services Principals this tutorial needs"
+  default = {
+    ecs_tasks = "ecs-tasks.amazonaws.com"
+    ecs       = "ecs.amazonaws.com"
+  }
+}
+
+variable "iam_role_name" {
+  type        = string
+  description = "Base name of the IAM role to create in this tutorial"
+  default     = "hashicups"
+}
+
+variable "iam_effect" {
+  type        = map(string)
+  description = "Allow or Deny for IAM policies"
+  default = {
+    allow = "Allow"
+    deny  = "Deny"
+  }
+}
+
+variable "iam_action_type" {
+  type        = map(string)
+  description = "Actions required for IAM roles in this tutorial"
+  default = {
+    assume_role = "sts:AssumeRole"
+  }
+}
+variable "iam_actions_allow" {
+  type        = map(any)
+  description = "What resources an IAM role is accessing in this tutorial"
+  default = {
+    secrets_manager_get = ["secretsmanager:GetSecretValue"]
+    logging_create_and_put = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+    "logs:PutLogEvent"]
+    elastic_load_balancer = ["elasticloadbalancing:*"]
+
+  }
+}
+
+variable "iam_logs_actions_allow" {
+  default = [
+    "logs:CreateLogGroup",
+    "logs:CreateLogStream",
+    "logs:PutLogEvent"
+  ]
+}
